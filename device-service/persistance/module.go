2cde181c7d12fe0d46b7ca904264a3d03a21f72a
baydinvladislav <baydinvladislav@yandex.ru>
2024-09-27 19:04:24 +0300

    uuid as primary keys

--- device-service/persistance/module.go
+++ device-service/persistance/module.go
@@ -2,14 +2,16 @@ package persistance
 
 import (
 	"github.com/google/uuid"
-	"gorm.io/gorm"
+	"time"
 )
 
 type ModuleModel struct {
-	ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	gorm.Model
-	Type        string `gorm:"size:50"`
-	Description string `gorm:"type:text"`
+	ID          uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	CreatedAt   time.Time  `gorm:"column:created_at"`
+	UpdatedAt   time.Time  `gorm:"column:updated_at"`
+	DeletedAt   *time.Time `gorm:"column:deleted_at"`
+	Type        string     `gorm:"size:50"`
+	Description string     `gorm:"type:text"`
 }
 
 func (ModuleModel) TableName() string {
@@ -17,11 +19,13 @@ func (ModuleModel) TableName() string {
 }
 
 type HouseModuleModel struct {
-	ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	gorm.Model
-	HouseID  uuid.UUID `gorm:"type:uuid;not null"`
-	ModuleID uuid.UUID `gorm:"type:uuid;not null"`
-	TurnOn   bool
+	ID        uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	CreatedAt time.Time  `gorm:"column:created_at"`
+	UpdatedAt time.Time  `gorm:"column:updated_at"`
+	DeletedAt *time.Time `gorm:"column:deleted_at"`
+	HouseID   uuid.UUID  `gorm:"type:uuid;not null"`
+	ModuleID  uuid.UUID  `gorm:"type:uuid;not null"`
+	TurnOn    bool
 }
 
 func (HouseModuleModel) TableName() string {
