914e759e04d9e1a0f89fe6d868f366c5ab36707f
baydinvladislav <baydinvladislav@yandex.ru>
2024-10-08 08:26:15 +0300

    saga user service

--- user-service/service/house_service.go
+++ user-service/service/house_service.go
@@ -1,13 +1,17 @@
 package service
 
 import (
+	"fmt"
 	"user-service/persistance"
 	web_schemas "user-service/presentation/web-schemas"
 	"user-service/repository"
+	"user-service/suppliers"
 )
 
 type HouseService struct {
-	repo repository.HouseRepository
+	repo          repository.HouseRepository
+	kafkaSupplier suppliers.KafkaSupplier
+	userService   UserService
 }
 
 func NewHouseService(repo repository.HouseRepository) *HouseService {
@@ -41,4 +45,39 @@ func (s *HouseService) GetUserHouses(userID uint) ([]web_schemas.HouseOut, error
 
 func (s *HouseService) UpdateUserHouse(house web_schemas.UpdateHouseIn) (*persistance.HouseModel, error) {
 	return s.repo.UpdateUserHouse(house)
+}
+
+func (s *HouseService) verifyUserAndHouse(userId uint, houseId uint) (bool, error) {
+	user, err := s.userService.GetRequiredById(userId)
+	if err != nil {
+		return false, err
+	}
+
+	if user.Username == "" {
+		return false, fmt.Errorf("user with ID %d does not have a username", userId)
+	}
+
+	houses, err := s.GetUserHouses(userId)
+	if err != nil {
+		return false, err
+	}
+
+	var foundHouse *web_schemas.HouseOut
+
+	for _, house := range houses {
+		if house.ID == houseId {
+			foundHouse = &house
+			break
+		}
+	}
+
+	if foundHouse.Square < 100 {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (s *HouseService) ApproveModuleInstallation(userId uint, houseId uint) (bool, error) {
+	return s.verifyUserAndHouse(userId, houseId)
 }
